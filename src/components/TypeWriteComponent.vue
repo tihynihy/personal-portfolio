084e52e422ebedd4cb394f8cc7ffef18e98ece8b
tihynihy <tihynihy@gmail.com>
2023-11-14 01:57:30 +0100

    mx-auto the typewrite components text

--- src/components/TypeWriteComponent.vue
+++ src/components/TypeWriteComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col border gap-4 bg-gray-800 rounded-xl border-white px-6 py-4">
-    <div class="line-1 text-white">
+    <div class="line-1 text-white mx-auto">
        Hi! I am Nihad :)
     </div>
 </div>
