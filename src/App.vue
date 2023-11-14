a1610aaebf002bf24517b79d587c268b988ff2b4
tihy.nihy <tihynihy@gmail.com>
2023-11-14 13:15:30 +0100

    adapt the timeline component to the portfolio

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
-<template>  
+<template>
     <particles-component />
     <!-- Navigation -->
-  <div class="flex items-center justify-center">
-    <navbar /> 
-  </div>
-  <!-- Body -->
-  <div class="flex flex-col items-center justify-center mt-10  border-white">
-
-    <avatar />
-    <div class="mt-10">
-    <typewrite-effect />
-    <about-component />
-</div>
-  </div>
- </template>
+    <div class="flex items-center justify-center">
+        <navbar />
+    </div>
+    <!-- Body -->
+    <div class="flex flex-col items-center justify-center mt-10">
+        <avatar />
+        <div class="mt-10">
+            <typewrite-effect />
+            <about-component />
+        </div>
+    </div>
+    <div class="bg-gray-800 text-white rounded-xl mx-auto px-4 py-2 w-[30%] border border-white my-10">
+        Experience Roadmap
+    </div>
+    <div class="w-[800px] mx-auto border border-white rounded-xl px-6">
+        <timeline-component />
+    </div>
+</template>
 
 <script>
 </script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ParticlesComponent from './components/ParticlesComponent.vue';
 import Particles from 'vue3-particles'
 import AboutComponent from './components/AboutComponent.vue';
 import TerminalComponent from './components/TerminalComponent.vue';
+import TimelineComponent from './components/TimelineComponent.vue';
 import TypeWriteComponent from './components/TypeWriteComponent.vue';
 import App from './App.vue'
 
@@ -15,6 +16,7 @@ app.component('navbar', NavBar);
 app.component('avatar', AvatarComponent);
 app.component('particles-component', ParticlesComponent);
 app.component('about-component', AboutComponent); 
+app.component('timeline-component', TimelineComponent);
 app.component('terminal-component',TerminalComponent);
 app.component('typewrite-effect', TypeWriteComponent);
   app.use(Particles)
