3bee53c649192257c9d79d29aea855b75408806f
tihynihy <tihynihy@gmail.com>
2023-11-14 01:47:39 +0100

    sky update

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="border flex items-center justify-center gap-10 py-4 w-[50%] rounded-xl mt-4 bg-gray-600">
         <button
-            class="hover:-translate-y-1 font-semibold text-white hover:text-white hover:scale-110 hover:bg-indigo-500 duration-300 rounded-xl px-4 py-2 transition ease-in-out delay-150">Portfolio</button>
+            class="hover:-translate-y-1 font-semibold text-white hover:text-white hover:scale-110 hover:bg-sky-500 duration-300 rounded-xl px-4 py-2 transition ease-in-out delay-150">Portfolio</button>
         <button
-            class="hover:-translate-y-1 font-semibold text-white hover:text-white hover:scale-110 hover:bg-indigo-500 duration-300 rounded-xl px-4 py-2 transition ease-in-out delay-150">
+            class="hover:-translate-y-1 font-semibold text-white hover:text-white hover:scale-110 hover:bg-sky-500 duration-300 rounded-xl px-4 py-2 transition ease-in-out delay-150">
             Experience Roadmap</button>
         <button
-            class="hover:-translate-y-1 font-semibold text-white hover:text-white hover:scale-110 hover:bg-indigo-500 duration-300 rounded-xl px-4 py-2 transition ease-in-out delay-150">Contact</button>
+            class="hover:-translate-y-1 font-semibold text-white hover:text-white hover:scale-110 hover:bg-sky-500 duration-300 rounded-xl px-4 py-2 transition ease-in-out delay-150">Contact</button>
     </div>
 </template>
 <script>
