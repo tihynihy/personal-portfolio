71dbe37aef7b1e4289d671a6b00d7c6156b72869
tihynihy <tihynihy@gmail.com>
2023-11-14 18:55:37 +0100

    patch

--- src/components/TimelineComponent.vue
+++ src/components/TimelineComponent.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="bg-gray-800">
-        <ol class="relative border-s border-gray-200 dark:border-gray-700">
+        <ol class="relative border-s border-gray-200">
             <li v-for="(item, index) in portfolioData" :key="index"
                 class="mb-10 ms-4 px-4 py-4 rounded-xl hover:bg-sky-700 text-white hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
                 <div
-                    class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700">
+                    class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white">
                 </div>
                 <time class="mb-1 text-sm font-normal leading-none">{{ item.date }}</time>
                 <h3 class="text-lg font-semibold">{{ item.title }}</h3>
                 <p class="mb-4 text-base font-normal">{{ item.description }}</p>
                 <a :href="item.link"
-                    class="inline-flex items-center px-4 py-2 text-sm font-medium bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 text-black dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                    class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-600 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none">
                     Learn more
                     <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                         fill="none" viewBox="0 0 14 10">
