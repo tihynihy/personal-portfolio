a07b142f30008add27d65f3027276c9a93aacf01
tihynihy <tihynihy@gmail.com>
2024-06-20 17:16:05 +0200

    navbar responsive

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -1,22 +1,20 @@
 <template>
-    <div class="flex items-center justify-center gap-10 py-4 w-[40%] rounded-bl-2xl rounded-br-2xl mt-0 backdrop-blur-sm bg-white/20 overflow-hidden ">
-        <router-link to="/">
-        <button
-            class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
-            Home
+    <div class="flex flex-wrap items-center justify-center gap-4 md:gap-10 py-4 w-full md:w-3/5 lg:w-2/5 rounded-bl-2xl rounded-br-2xl mt-0 backdrop-blur-sm bg-white/20 overflow-hidden">
+      <router-link to="/">
+        <button class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
+          Home
         </button>
-    </router-link>
-        <router-link to="/roadmap">
-            <button
-            class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
-            Experience Roadmap
+      </router-link>
+      <router-link to="/roadmap">
+        <button class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
+          Experience Roadmap
         </button>
-    </router-link>
-    <router-link to="/about">
-        <button
-            class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
-            About me
+      </router-link>
+      <router-link to="/about">
+        <button class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
+          About me
         </button>
-    </router-link>
+      </router-link>
     </div>
-</template>
+  </template>
+  
