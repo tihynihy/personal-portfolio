713b8e55803bd70bfdb6b7d9b940b1c4cbff3085
tihynihy <tihynihy@gmail.com>
2024-06-21 22:10:32 +0200

    navbar fix

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -7,12 +7,12 @@
       </router-link>
       <router-link to="/roadmap">
         <button class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
-          Experience Roadmap
+          Roadmap
         </button>
       </router-link>
       <router-link to="/about">
         <button class="hover:-translate-y-1 text-white hover:text-white hover:scale-110 hover:bg-gray-600 duration-300 rounded-3xl px-4 py-2 transition ease-in-out delay-150">
-          About me
+          Tech Experience
         </button>
       </router-link>
     </div>
