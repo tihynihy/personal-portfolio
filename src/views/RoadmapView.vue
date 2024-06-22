54dcf02eab8e05c79bf76e4ae6816f71e580ede0
tihynihy <tihynihy@gmail.com>
2024-06-22 11:25:35 +0200

    roadmap patch

--- src/views/RoadmapView.vue
+++ src/views/RoadmapView.vue
@@ -1,28 +1,28 @@
 <template>
   <particles-component />
-  <div class="items-center justify-center flex flex-col">
-    <navbar-component />
-    <div class="mt-32">
-      <div class="px-40 mt-[5%]">
-        <div>
-          <TimelineComponent :items="items">
-            <!-- Your SVG content goes here -->
-            <svg
-              class="w-4 h-4 text-blue-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                d="M10 20a10 10 0 100-20 10 10 0 000 20zm1-15v4H7v2h4v4h2v-4h4V9h-4V5h-2z"
-              />
-            </svg>
-          </TimelineComponent>
+    <div class="items-center justify-center flex flex-col">
+      <navbar-component />
+      <div class="mt-32">
+        <div class="px-40 mt-[5%]">
+          <div>
+            <TimelineComponent :items="items">
+              <!-- Your SVG content goes here -->
+              <svg
+                class="w-4 h-4 text-blue-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  d="M10 20a10 10 0 100-20 10 10 0 000 20zm1-15v4H7v2h4v4h2v-4h4V9h-4V5h-2z"
+                />
+              </svg>
+            </TimelineComponent>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 </template>
 <script>
 import TimelineComponent from "../components/TimelineComponent.vue";
