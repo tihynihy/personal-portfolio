99f25cfd388acde49f31c265598e8b8496c6f379
tihynihy <tihynihy@gmail.com>
2024-07-09 20:09:50 +0200

    navbar patch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="items-center justify-center flex flex-col sticky top-0">
-    <navbar-component />
-  </div>
-  <router-view></router-view>
-</template>
+    <div>
+      <div class="navbar-container items-center justify-center flex flex-col sticky top-0">
+        <navbar-component />
+      </div>
+      <div class="content-container">
+        <router-view />
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .navbar-container {
+    z-index: 1000; 
+  }
+  </style>
+  
