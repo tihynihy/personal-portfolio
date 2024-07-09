4f7e2b6ca9c941bda349817549d3d056487f2b9e
tihynihy <tihynihy@gmail.com>
2024-07-09 16:38:52 +0200

    patch

--- src/views/TechView.vue
+++ src/views/TechView.vue
@@ -1,15 +1,15 @@
 <template>
   <particles-component />
-    <div class="items-center justify-center flex flex-col">
-      <navbar-component />
-      <div class="mt-32">
-        <div class="px-40 mt-[5%]">
-          <div>
-            <TimelineComponent :items="items" />
-          </div>
+  <div class="items-center justify-center flex flex-col">
+    <navbar-component />
+    <div class="mt-32">
+      <div class="px-40 mt-[5%]">
+        <div>
+          <TimelineComponent :items="items" />
         </div>
       </div>
     </div>
+  </div>
 </template>
 <script>
 import TimelineComponent from "../components/TimelineComponent.vue";
@@ -22,9 +22,9 @@ export default {
     return {
       items: [
         {
-          title: 'University Basics',
-          releaseDate: 'December 2, 2021',
-          description: 'Started my journey in coding with the basics.',
+          title: "University Basics",
+          releaseDate: "December 2, 2021",
+          description: "Started my journey in coding with the basics.",
           svg: `
             <svg class="w-4 h-4 text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 20a10 10 0 100-20 10 10 0 000 20zm1-15v4H7v2h4v4h2v-4h4V9h-4V5h-2z"/>
@@ -32,9 +32,10 @@ export default {
           `,
         },
         {
-          title: 'Hackathons',
-          releaseDate: 'December 23, 2021',
-          description: 'Winner of the BEST 2022 Hackathon in Sarajevo | BEST Mostar Hackathon 2021.',
+          title: "Hackathons",
+          releaseDate: "December 23, 2021",
+          description:
+            "Winner of the BEST 2022 Hackathon in Sarajevo | BEST Mostar Hackathon 2021.",
           svg: `
             <svg class="w-4 h-4 text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 20a10 10 0 100-20 10 10 0 000 20zm1-15v4H7v2h4v4h2v-4h4V9h-4V5h-2z"/>
@@ -42,9 +43,10 @@ export default {
           `,
         },
         {
-          title: 'Frontend Development',
-          releaseDate: 'January 5, 2022',
-          description: 'Worked full time as a Vue.js developer | Figma UI/UX development',
+          title: "Frontend Development",
+          releaseDate: "January 5, 2022",
+          description:
+            "Worked full time as a Vue.js developer | Figma UI/UX development",
           svg: `
             <svg class="w-4 h-4 text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 20a10 10 0 100-20 10 10 0 000 20zm1-15v4H7v2h4v4h2v-4h4V9h-4V5h-2z"/>
