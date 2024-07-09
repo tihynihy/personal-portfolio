f8dec6a24fcf14b7f60f3c291171125419f65f9d
tihynihy <tihynihy@gmail.com>
2024-07-09 18:00:17 +0200

    clean c

--- src/main.js
+++ src/main.js
@@ -1,41 +1,40 @@
-import { createApp } from 'vue'
-import router from './router';
-import './style.css'
-import NavBarComponent from './components/NavBarComponent.vue';
-import AvatarComponent from './components/AvatarComponent.vue';
-import ParticlesComponent from './components/ParticlesComponent.vue';
-import Particles from 'vue3-particles'
-import AboutComponent from './components/AboutComponent.vue';
-import TerminalComponent from './components/TerminalComponent.vue';
-import TimelineComponent from './components/TimelineComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
-import ContactView from './views/ContactView.vue';
-import TypeWriteComponent from './components/TypeWriteComponent.vue';
-import ToastComponent from './components/ToastComponent.vue';
-import BaseView from './views/BaseView.vue';
-import HomeViewComponent from './views/HomeView.vue';
-import TechView from './views/TechView.vue';
-import GridComponent from './components/GridComponent.vue';
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import "./style.css";
+import NavBarComponent from "./components/NavBarComponent.vue";
+import AvatarComponent from "./components/AvatarComponent.vue";
+import ParticlesComponent from "./components/ParticlesComponent.vue";
+import Particles from "vue3-particles";
+import AboutComponent from "./components/AboutComponent.vue";
+import TerminalComponent from "./components/TerminalComponent.vue";
+import TimelineComponent from "./components/TimelineComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import ContactView from "./views/ContactView.vue";
+import TypeWriteComponent from "./components/TypeWriteComponent.vue";
+import ToastComponent from "./components/ToastComponent.vue";
+import BaseView from "./views/BaseView.vue";
+import HomeViewComponent from "./views/HomeView.vue";
+import TechView from "./views/TechView.vue";
+import GridComponent from "./components/GridComponent.vue";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.component('navbar-component', NavBarComponent);
-app.component('avatar', AvatarComponent);
-app.component('particles-component', ParticlesComponent);
-app.component('about-component', AboutComponent); 
-app.component('timeline-component', TimelineComponent);
-app.component('terminal-component',TerminalComponent);
-app.component('footer-component', FooterComponent);
-app.component('typewrite-effect', TypeWriteComponent);
-app.component('contact-view', ContactView);
-app.component('toast-component', ToastComponent);
-app.component('homeview-component', HomeViewComponent);
-app.component('baseview-component', BaseView);
-app.component('grid-component', GridComponent);
-app.component('techview-component', TechView);
+app.component("navbar-component", NavBarComponent);
+app.component("avatar", AvatarComponent);
+app.component("particles-component", ParticlesComponent);
+app.component("about-component", AboutComponent);
+app.component("timeline-component", TimelineComponent);
+app.component("terminal-component", TerminalComponent);
+app.component("footer-component", FooterComponent);
+app.component("typewrite-effect", TypeWriteComponent);
+app.component("contact-view", ContactView);
+app.component("toast-component", ToastComponent);
+app.component("homeview-component", HomeViewComponent);
+app.component("baseview-component", BaseView);
+app.component("grid-component", GridComponent);
+app.component("techview-component", TechView);
 
-  app.use(Particles)
-  app.use(router)
-  app.mount('#app')
-
+app.use(Particles);
+app.use(router);
+app.mount("#app");
